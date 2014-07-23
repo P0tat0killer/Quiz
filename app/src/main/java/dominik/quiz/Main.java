86f0358cda0dbd24294b2236148d8e9655f0b0c6
meinusername <dominik_meyer99@web.de>
2014-07-23 17:14:55 +0200

    SQL_Database

--- app/src/main/java/dominik/quiz/Main.java
+++ app/src/main/java/dominik/quiz/Main.java
@@ -4,18 +4,66 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Main extends Activity {
 
+    Button BTN_ANTWORT_A;
+    Button BTN_ANTWORT_B;
+    Button BTN_ANTWORT_C;
+    Button BTN_ANTWORT_D;
+    Sql_datenbank sql;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sql = new Sql_datenbank(this);
+         BTN_ANTWORT_A = (Button) findViewById(R.id.Antwort_A);
+        BTN_ANTWORT_B = (Button) findViewById(R.id.Antwort_B);
+        BTN_ANTWORT_C = (Button) findViewById(R.id.Antwort_C);
+        BTN_ANTWORT_D =  (Button) findViewById(R.id.Andwort_D);
 
-        
+        BTN_ANTWORT_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == BTN_ANTWORT_A){
+
+                }
+            }
+        });
+        BTN_ANTWORT_B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == BTN_ANTWORT_B){
+
+                }
+            }
+        });
+        BTN_ANTWORT_C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == BTN_ANTWORT_C){
+
+                }
+            }
+        });
+        BTN_ANTWORT_D.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v == BTN_ANTWORT_D){
+
+                }
+            }
+        });
+
+
+
+
+
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
